b1ca5c1df69ffaf70691e8e502ef7d1763e4bd41
ASUS-ПК\ASUS <aniasatryan336@gmail.com>
2021-02-19 00:40:42 +0400

    continue

--- js/slider.js
+++ js/slider.js
@@ -1,94 +1,53 @@
-const mainImg = document.querySelector("#mainImg");
+const mainImage = document.querySelector("#mainImg");
+const thumbContainer = document.querySelector("#thumb_img_list");
+const slideContainer = document.querySelector(".slide_container");
+const slideshowContainer = document.querySelector(".slideshow_container");
+const closeButtonContainer = document.querySelector(".close_btn_container");
 
-const thumb1 = document.querySelector("#thumb1");
-const thumb1Src = document.querySelector("#thumb1").src;
-const thumb2 = document.querySelector("#thumb2");
-const thumb2Src = document.querySelector("#thumb2").src;
-const thumb3 = document.querySelector("#thumb3");
-const thumb3Src = document.querySelector("#thumb3").src;
-const thumb4 = document.querySelector("#thumb4");
-const thumb4Src = document.querySelector("#thumb4").src;
-const thumb5 = document.querySelector("#thumb5");
-const thumb5Src = document.querySelector("#thumb5").src;
-const thumb6 = document.querySelector("#thumb6");
-const thumb6Src = document.querySelector("#thumb6").src;
+thumbContainer.addEventListener("click", (event)=> {
+	let targetElement = event.target;
 
-
-thumb1.addEventListener("click", ()=> {
-	mainImg.src = thumb1Src;
-});
-
-thumb2.addEventListener("click", ()=> {
-	mainImg.src = thumb2Src;
-});
-
-thumb3.addEventListener("click", ()=> {
-	mainImg.src = thumb3Src;
-});
-
-thumb4.addEventListener("click", ()=> {
-	mainImg.src = thumb4Src;
-});
-
-thumb5.addEventListener("click", ()=> {
-	mainImg.src = thumb5Src;
-});
-
-thumb6.addEventListener("click", ()=> {
-	mainImg.src = thumb6Src;
+	if (targetElement.tagName === "IMG") {
+		mainImage.src = targetElement.getAttribute("src");
+	}
+    
+    slideshowContainer.style.display = "block";
+    slideContainer.style.display = "block";
+    closeButtonContainer.style.display = "block";
 });
 
 
 const closeBtn = document.querySelector("#closebtn");
-const updateBtn = document.querySelector("#updateBtn");
-const slideContainer = document.querySelector(".slide_container");
 
 closeBtn.addEventListener("click", ()=> {
 	slideContainer.style.display = "none";
+	slideshowContainer.style.display = "none";
 });
 
-updateBtn.addEventListener("click", ()=> {
-	slideContainer.style.display = "block";
-});
-
-const slide = document.querySelector(".slides");
-const images = document.querySelectorAll(".slides img");
 
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
+const images = document.querySelectorAll(".slides img");
 
-let counter = 1;
-const size = images[0].clientWidth;
-
-slide.style.transform = "translateX(" + (-size * counter) + "px)";    
+let i = images.length;
 
 nextBtn.addEventListener("click", ()=> {
-	if(counter >= images.length - 1) return;
-	slide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    slide.style.transform = "translateX(" + (-size * counter) + "px)";
+	if (i < images.length) {
+		i++;
+	} else {
+		i = 1;
+	}
+
+	mainImage.src = images[i - 1].src;
 });
+
 
 prevBtn.addEventListener("click", ()=> {
-	if(counter <= 0) return;
-	slide.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    slide.style.transform = "translateX(" + (-size * counter) + "px)";
+	if (i < images.length + 1 && i > 1) {
+		i--;
+	} else {
+		i = images.length;
+	}
+
+	mainImage.src = images[i - 1].src;
 });
-
-slide.addEventListener("transitionend", ()=> {
-    if(images[counter].id === "lastImage") {
-    	slide.style.transition = "none";
-    	counter = images.length - 2;
-    	slide.style.transform = "translateX(" + (-size * counter) + "px)";
-    }
-
-    if(images[counter].id === "firstImage") {
-    	slide.style.transition = "none";
-    	counter = images.length - counter;
-    	slide.style.transform = "translateX(" + (-size * counter) + "px)";
-    }
-});
-
-
-
